3d80764cf1b3f910e128ac1f9c6ba8c87e3f1e08
Tharunkumar <40721166+tharunkumarnataraj@users.noreply.github.com>
2026-03-19 11:52:50 +0530

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,105 +1,38 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("androidx.navigation.safeargs")
-    id("kotlin-kapt")
 }
 
-val app_name = "Bluetooth LE Spam"
-
 android {
-    namespace "com.example.blespam"
+    namespace = "com.example.blespam"
     compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.blespam"
         minSdk = 21
         targetSdk = 34
-    }
-    signingConfigs {
-        create("release") {
-            storeFile = file("release.jks")
-            storePassword = System.getenv("STORE_PASSWORD")
-            keyAlias = System.getenv("KEY_ALIAS")
-            keyPassword = System.getenv("KEY_PASSWORD")
-        }
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
-        configureEach {
-            signingConfig = signingConfigs["release"]
-        }
-        release {
-            resValue("string", "app_name", app_name)
+        getByName("debug") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            resValue("string", "app_name", "$app_name Debug")
-            applicationIdSuffix = ".debug"
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
-
 dependencies {
-    implementation("com.airbnb.android:lottie:6.3.0")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    //testImplementation("junit:junit:4.13.2")
-    //androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    //androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.preference:preference-ktx:1.2.1")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-    val room_version = "2.6.1"
-    implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
-
-    // To use Kotlin annotation processing tool (kapt)
-    kapt("androidx.room:room-compiler:$room_version")
-
-    // To use Kotlin Symbol Processing (KSP)
-    //ksp("androidx.room:room-compiler:$room_version")
-
-    // optional - Kotlin Extensions and Coroutines support for Room
-    //implementation("androidx.room:room-ktx:$room_version")
-
-    // optional - RxJava2 support for Room
-    //implementation("androidx.room:room-rxjava2:$room_version")
-
-    // optional - RxJava3 support for Room
-    implementation("androidx.room:room-rxjava3:$room_version")
-
-    // optional - Guava support for Room, including Optional and ListenableFuture
-    //implementation("androidx.room:room-guava:$room_version")
-
-    // optional - Test helpers
-    //testImplementation("androidx.room:room-testing:$room_version")
-
-    // optional - Paging 3 Integration
-    //implementation("androidx.room:room-paging:$room_version")
-    
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
 }
